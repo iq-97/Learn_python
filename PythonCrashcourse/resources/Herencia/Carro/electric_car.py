3c88cd1e715734be23130d50186ecd28ea756cd5
iq231097 <hfelipec@miumg.edu.gt>
2018-07-10 12:04:49 -0600

    agregando archivos en la carpeta Herencia/carro

--- PythonCrashcourse/resources/Herencia/Carro/electric_car.py
+++ PythonCrashcourse/resources/Herencia/Carro/electric_car.py
@@ -1,16 +1,39 @@
 import car
-class carro_electrico(car.car):
+
+class CarroElectrico(car.car):
     """a simple attempt to represent a electric car"""
-
-    def __init__(self, make,model,year):
+    def __init__(self, make, model, year):
         super().__init__(make, model, year)
-        self.bateria = 70
+        self.bateria = Battery()
 
-    def ver_carga(self):
-        print("la duracion de bateria es de : "+str(self.bateria))
+   # def ver_carga(self):
+         # print("la duracion de bateria es de : "+str(self.bateria))
+# instances as attributes
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self,battery_size=85):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
 
-tesla =carro_electrico('tesla','z 1','2018')
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a "+str(self.battery_size)+"-KWh battery.")
+
+    def get_range(self):
+        """print a statement about the range this battery provides"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "this car can go approximately "+ str(range)
+        message += " miles on a full charge."
+        print(message)
+
+
+tesla =CarroElectrico('tesla', 'z 1', '2018')
 print(tesla.descriptive_name())
-tesla.ver_carga()
+tesla.bateria.describe_battery()
+tesla.bateria.get_range()
 tesla.update_odometer_read(80)
 tesla.read_odometer()
---
+++ PythonCrashcourse/resources/Herencia/Carro/my_car.py
@@ -0,0 +1,7 @@
+from car import car
+# importando del modulo car la clase car
+
+# instanciando objeto
+my_new_car = car('audi', 'a4', '2015')
+print(my_new_car.descriptive_name())
+
