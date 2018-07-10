f34ed59a9c6f98df3a0313197c3aa8db5a7da28b
iq231097 <hfelipec@miumg.edu.gt>
2018-07-10 02:02:17 -0600

    modificando ejemplo de herencia

--- PythonCrashcourse/resources/Herencia/Carro/car.py
+++ PythonCrashcourse/resources/Herencia/Carro/car.py
@@ -10,9 +10,7 @@ class car():
     def descriptive_name(self):
         """return a neatly formatted descriptive name"""
         long_name = self.model+" "+self.make+" "+self.year+"."
-        return long_name.title()
-
-
+        return long_name
     def read_odometer(self):
         """devuelve la lectura de kilometros recorridos"""
         print("el total de kilometros recorridos es de: "+str(self.odometer))
@@ -29,12 +27,3 @@ class car():
     def increment_odometer_read(self, miles):
         """incrementa la lectura de kilometraje"""
         self.odometer += miles
-#my_new_car = car('ford','focus','1968')
-#cambiando el valor por defecto de odometer directamente despues de la instancia
-#my_new_car.odometer = 23
-#cambiando el valor por defecto de odometer utilizando un metodo
-#my_new_car.update_odometer_read(150)
-#my_new_car.read_odometer()
-#my_new_car.increment_odometer_read(250)
-#my_new_car.read_odometer()
-#print(my_new_car.descriptive_name())
--- PythonCrashcourse/resources/Herencia/Carro/electric_car.py
+++ PythonCrashcourse/resources/Herencia/Carro/electric_car.py
@@ -1,44 +1,16 @@
+import car
+class carro_electrico(car.car):
+    """a simple attempt to represent a electric car"""
 
-class car():
-    """A simple attempt to represent a car"""
-
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer = 0
-
-    def descriptive_name(self):
-        """return a neatly formatted descriptive name"""
-        long_name = self.model+" "+self.make+" "+self.year+"."
-        return long_name.title()
-
-
-    def read_odometer(self):
-        """devuelve la lectura de kilometros recorridos"""
-        print("el total de kilometros recorridos es de: "+str(self.odometer))
-
-    #creando metodo para modificar valor de read_odometer
-    def update_odometer_read(self, recorrido):
-        """modifica la lectura de kilometros recorridos"""
-        if recorrido >= self.odometer:
-            self.odometer = recorrido
-        else:
-            print("no puedes retroceder el conteo de kilometros recorridos")
-
-    #creando metodo para incrementar kilometraje
-    def increment_odometer_read(self, miles):
-        """incrementa la lectura de kilometraje"""
-        self.odometer += miles
-
-class electric_car(car):
-    def _init_(self, make, model,year):
-        super()._init_(make, model, year)
+    def __init__(self, make,model,year):
+        super().__init__(make, model, year)
         self.bateria = 70
 
-    def tama_bateria(self):
-        """a attempt to describe the size of the battery of a car"""
-        print("el tamanio de la bateria es de: "+str(self.bateria))
-tesla = electric_car('tesla','model s','2018')
+    def ver_carga(self):
+        print("la duracion de bateria es de : "+str(self.bateria))
+
+tesla =carro_electrico('tesla','z 1','2018')
 print(tesla.descriptive_name())
-tesla.tama_bateria()
+tesla.ver_carga()
+tesla.update_odometer_read(80)
+tesla.read_odometer()
